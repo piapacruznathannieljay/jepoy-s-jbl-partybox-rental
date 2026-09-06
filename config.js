93de4fd1236b6392c80dc45850a2e7313bee2923
piapacruznathannieljay <piapacruznathannieljay@gmail.com>
2026-09-06 17:08:03 +0800

    Update Supabase URL and anon key in config.js
    Updated Supabase configuration with actual URL and anon key.

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ SUPABASE CONFIGURATION
 =========================================================
 */
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
+const SUPABASE_URL = "https://mxercqhnytshxmnoyoja.supabase.co";
 
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_ANON_KEY = "sb_publishable_3VccmXrJiyEbLWxdWZSj5g_1GXSr9ni";
 
 
 /*
